c1b480f676989d22caa2a476e910646e00282905
dkurukula <dimuthkurukula@gmail.com>
2019-06-21 13:14:17 -0400

    set year as variable
    set year as variable, can now call both year using YEARN and month using MONTHN

--- getdate.bat
+++ getdate.bat
@@ -4,7 +4,8 @@ FOR /F "skip=1 tokens=1-6" %%A IN ('WMIC Path Win32_LocalTime Get Day^,Hour^,Min
         SET /A FDATE=%%F*10000+%%D*100+%%A
     )
 )
+SET /A YEARN=%FDATE:~0,4% 
 SET /A MONTHN=10000%FDATE:~4,2% %% 10000
 echo date=%FDATE%
-echo year=%FDATE:~0,4%
+echo year=%YEARN%
 echo month = %MONTHN%
