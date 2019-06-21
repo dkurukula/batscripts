dab05f2103a690794d939ae878c77d35cd4aad59
dkurukula <dimuthkurukula@gmail.com>
2019-06-21 13:02:56 -0400

    Create getdate.bat
    based on this thread, modified to provide integer to use as cli argument in python

---
+++ getdate.bat
@@ -0,0 +1,10 @@
+@echo off
+FOR /F "skip=1 tokens=1-6" %%A IN ('WMIC Path Win32_LocalTime Get Day^,Hour^,Minute^,Month^,Second^,Year /Format:table') DO (
+    if "%%B" NEQ "" (
+        SET /A FDATE=%%F*10000+%%D*100+%%A
+    )
+)
+SET /A MONTHN=10000%FDATE:~4,2% %% 10000
+echo date=%FDATE%
+echo year=%FDATE:~0,4%
+echo month = %MONTHN%
